3d29cf689c2bf8d2204be42541d9b68d55882f1e
Arreis <arreis.develop@gmail.com>
2021-12-30 23:12:43 +0100

    Modified BrightnessLevel to allow interoperability with Java

--- domain/src/main/java/com/arreis/brightnessswitcher/domain/entity/BrightnessLevel.kt
+++ domain/src/main/java/com/arreis/brightnessswitcher/domain/entity/BrightnessLevel.kt
@@ -1,6 +1,8 @@
 package com.arreis.brightnessswitcher.domain.entity
 
-sealed class BrightnessLevel {
-    object Auto: BrightnessLevel()
-    class FixedValue(private val value: Int): BrightnessLevel()
+sealed class BrightnessLevel(open val value: Double) {
+
+    data class Auto(override val value: Double = -1.0): BrightnessLevel(value)
+
+    data class FixedValue(override val value: Double): BrightnessLevel(value)
 }
